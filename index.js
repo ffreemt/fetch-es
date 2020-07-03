84a3567e348e88e9d9e0c9bcc6e539135cee3ec0
ffreemt <yucongo+fmt@gmail.com>
2020-07-04 00:39:15 +0800

    index.js and test

---
+++ index.js
@@ -0,0 +1,48 @@
+// query = nspell_text(query)
+// r = search_es(q, index)
+// if
+
+// import consola from "consola";
+const consola = require('consola');
+consola.level = process.env.CONSOLA_DEBUG || 3;
+// consola.level = process.env.CONSOLA_DEBUG || 5; //show debug
+
+const search_es = require("./search_es");
+const nspell_text = require("./nspell_text");
+const suggest_es = require("./suggest_es");
+
+
+module.exports = fetch_es
+async function fetch_es(query, index="") {
+  query = nspell_text(query);
+  if (!query) {
+    return [];
+  }
+
+  // default indices
+  if (!index) {
+    index = ["yhdcd", "dictcor", "uncor"];
+  }
+
+  let res;
+  try {
+    res = search_es(query, index);
+  } catch (e) {
+    consola.error("search_es e:", e);
+    res = [e.message];
+    return res;
+  }
+
+  if (res.length)
+    return res;
+
+  try {
+    res = suggest_es(query, index);
+  } catch (e) {
+    consola.error(`suggest_es e: ${e}`);
+    res = [e.message];
+  }
+
+  return res;
+
+};
---
+++ test/index.test.js
@@ -0,0 +1,40 @@
+// import consola from "consola";
+const consola = require('consola');
+consola.level = process.env.CONSOLA_DEBUG || 3;
+// consola.level = process.env.CONSOLA_DEBUG || 5; //show debug
+
+// -r esm
+// import fetch_es from "../index";
+const fetch_es = require("../index");
+import { expect } from "chai";
+
+describe("@1 index: sanity test ", () => {
+  context(" empty query ", () => {
+    it("#1 fetch_es(''): 'empty'", async () => {
+      var result = await fetch_es("");
+      consola.debug(`debug #1: **${result}**`);
+      expect(result.length).to.equal(0);
+    });
+  });
+
+  let query = "tett";
+  context(` query: ${query}`, () => {
+    it("#1 fetch_es(tett): ", async () => {
+      var result = await fetch_es(query);
+      consola.debug(`consola.debug #1: **${result}**`);
+      // console.log(`console.debug #1: **${result}**`);
+      expect(result.length).to.equal(10);
+    });
+  });
+
+  query = "test";
+  context(` query: ${query}`, () => {
+    it("#1 fetch_es(tett): ", async () => {
+      var result = await fetch_es(query);
+      consola.debug(`consola.debug #1: **${result}**`);
+      // console.log(`console.debug #1: **${result}**`);
+      expect(result.length).to.equal(10);
+    });
+  });
+
+});
